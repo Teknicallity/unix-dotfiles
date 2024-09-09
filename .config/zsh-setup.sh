71b24fabb382afd79cd0c19e5ab6ee8c876f59b2
Teknicallity <joshua.s.3322@gmail.com>
2024-09-09 18:58:35 -0400

    Fixed zsh setup ordering

--- .config/zsh-setup.sh
+++ .config/zsh-setup.sh
@@ -5,10 +5,13 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.c
 
 # from https://medium.com/@simontoth/best-way-to-manage-your-dotfiles-2c45bb280049
 cd ~ || exit
-alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles-git/ --work-tree=$HOME'
-echo "alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles-git/ --work-tree=$HOME'" >> ~/.zshrc
 
 mv ~/.zshrc ~/.zshrc.bak
+mv ~/.vimrc ~/.vimrc.bak
+mv ~/.nanorc ~/.nanorc.bak
+
+echo "alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles-git/ --work-tree=$HOME'" >> ~/.zshrc
+alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles-git/ --work-tree=$HOME'
 
 git clone --bare git@github.com:Teknicallity/unix-dotfiles.git "$HOME"/.dotfiles-git
 dotfiles config --local status.showUntrackedFiles no
