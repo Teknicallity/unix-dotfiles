06542fb0864203063b4f8dfa7ba8da77f4934664
Teknicallity <joshua.s.3322@gmail.com>
2024-09-09 19:58:03 -0400

    Add support for dotfile repo in bash

--- .config/shell_setup.sh
+++ .config/shell_setup.sh
@@ -6,13 +6,17 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.c
 # from https://medium.com/@simontoth/best-way-to-manage-your-dotfiles-2c45bb280049
 cd ~ || exit
 
-mv ~/.zshrc ~/.zshrc.bak
+if command -v zsh > /dev/null 2>&1; then
+    mv ~/.zshrc ~/.zshrc.bak
+    echo "alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles-git/ --work-tree=$HOME'" >> ~/.zshrc
+else
+    echo "alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles-git/ --work-tree=$HOME'" >> ~/.bash_aliases
+fi
+
 mv ~/.vimrc ~/.vimrc.bak
 mv ~/.nanorc ~/.nanorc.bak
 
-echo "alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles-git/ --work-tree=$HOME'" >> ~/.zshrc
-alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles-git/ --work-tree=$HOME'
-
 git clone --bare git@github.com:Teknicallity/unix-dotfiles.git "$HOME"/.dotfiles-git
+alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles-git/ --work-tree=$HOME'
 dotfiles config --local status.showUntrackedFiles no
 dotfiles checkout
